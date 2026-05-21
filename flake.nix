82335d42b967fd541c7606d3211e41a61eea1aee
Daste <stefankar1000@gmail.com>
2026-05-21 15:36:19 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }:
+    let
+      eachSystem =
+        f: nixpkgs.lib.genAttrs (import systems) (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            python314
+            python314Packages.uv
+          ];
+          shellHook = ''
+            uv sync --frozen --no-install-project --group dev
+          '';
+        };
+      });
+    };
+}
