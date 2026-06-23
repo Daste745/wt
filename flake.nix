2287e0ab72a6604f41ef60d04b391c9cd58bf158
Daste <stefankar1000@gmail.com>
2026-06-23 15:48:44 +0200

    Package using uv+nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { systems, nixpkgs, ... }:
+    {
+      systems,
+      nixpkgs,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
     let
       eachSystem =
         f: nixpkgs.lib.genAttrs (import systems) (system: f (import nixpkgs { inherit system; }));
@@ -24,6 +46,12 @@
           shellHook = ''
             uv sync --frozen --no-install-project --group dev
           '';
+        };
+      });
+      packages = eachSystem (pkgs: {
+        wt = pkgs.callPackage ./package.nix {
+          inherit uv2nix pyproject-nix pyproject-build-systems;
+          python = pkgs.python314;
         };
       });
     };
--- package.nix
+++ package.nix
@@ -1,11 +1,32 @@
 {
+  lib,
+  callPackage,
   python,
+  uv2nix,
   pyproject-nix,
+  pyproject-build-systems,
 }:
 let
-  project = pyproject-nix.lib.project.loadPyproject {
-    projectRoot = ./.;
+  inherit (callPackage pyproject-nix.build.util { }) mkApplication;
+
+  workspace = uv2nix.lib.workspace.loadWorkspace {
+    workspaceRoot = ./.;
   };
-  pkgAttrs = project.renderers.buildPythonPackage { inherit python; };
+
+  pyprojectOverlay = workspace.mkPyprojectOverlay {
+    sourcePreference = "wheel";
+  };
+
+  baseSet = callPackage pyproject-nix.build.packages {
+    inherit python;
+  };
+  overlays = [
+    pyproject-build-systems.overlays.default
+    pyprojectOverlay
+  ];
+  pythonSet = baseSet.overrideScope (lib.composeManyExtensions overlays);
 in
-python.pkgs.buildPythonApplication pkgAttrs
+mkApplication {
+  venv = pythonSet.mkVirtualEnv "wt-env" workspace.deps.default;
+  package = pythonSet.wt;
+}
